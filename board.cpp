421d7b87e29a84225da72a7397004e11acbe8a58
David Huson <dhuson@davidhuson.dev>
2023-02-02 19:12:23 -0600

    fixed the segfault bug

--- board.cpp
+++ board.cpp
@@ -1,11 +1,32 @@
 #include "board.hpp"
 #include <iostream>
 
+// Board::Board(): size(0), playingField(10, std::vector<Cell>(10)){}
 
-Board::Board(int size): size(size){
-  // Create a blank field of the appropriate size;
+// Board::Board(int size): size(size), playingField(size, std::vector<Cell>(size)){
+
+//   for(int i = 0; i < size; i++){
+//     for(int j = 0; j < size; j++){
+//       // printf("Accessing playingField at row: %d\n", i);
+//       playingField.at(i).push_back(Cell());
+//       playingField.at(i).at(j).setPlayerToken("X");
+//       std::string str = playingField.at(i).at(j).getStatus();
+//       std::cout << "Cell status: " << str << '|' << std::endl;
+//     }
+//   }
+// }
+
+Board::Board(int size): size(size) {
+  playingField = new Cell*[size];
+  for (int i = 0; i < size; ++i) 
+    playingField[i] = new Cell[size];
+}
+
+Board::~Board() {
+  for (int i = 0; i < size; i++)
+    delete [] playingField[i];
   
-  std::vector<std::vector<Cell>> playingField(size, std::vector<Cell>(size));
+  delete [] playingField;
 }
 
 
@@ -28,21 +49,19 @@ Board::Board(int size): size(size){
  */
 void Board::draw() {
   const std::string hBorderStart = "+";
-  const std::string vBorderStart = "| ";
   const std::string hBorder = "---+";
-  const std::string vBorder = " | ";
+  const std::string vBorder = "|";
 
   std::string board = "";
-  
+  // std::cout << playingField.at(0).at(0).getStatus() << std::endl;
   for(int i = 0; i < size; i++){
     board += hBorderStart + repeat(hBorder, size);
-    board += vBorderStart + ' ';
     for(int j = 0; j < size; j++){
       // printf("Accessing playingField at row, col: %d, %d\n", i, j);
       // std::string cellStatus = playingField.at(i).at(j).getStatus();
-      board += vBorder + ' ';
+      board += vBorder + " " + playingField[i][j].getStatus() + " ";
     }
-    board += '\n';
+    board += vBorder + '\n';
   }
   board += hBorderStart + repeat(hBorder, size);
 
@@ -57,3 +76,16 @@ std::string Board::repeat(const std::string str, int num){
   ret += '\n';
   return ret;
 }
+
+/*
+
++---+---+---+---+
+|  |   |   |   |  
++---+---+---+---+
+|  |   |   |   |  
++---+---+---+---+
+|  |   |   |   |  
++---+---+---+---+
+|  |   |   |   |  
++---+---+---+---+
+*/
--- board.hpp
+++ board.hpp
@@ -4,16 +4,18 @@
 #include "cell.hpp"
 #include <vector>
 class Board {
+    int size;
+    // std::vector<std::vector<Cell>> playingField;
+    Cell** playingField;
+    std::string repeat(const std::string str, int num);
   public:
+    Board();
+    ~Board();
     Board(int size);
     bool place(int col);
     bool checkForWin();
     void draw();
-  private:
-    // Cell** playingField;
-    std::vector<std::vector<Cell>> playingField;
-    int size;
-    std::string repeat(const std::string str, int num);
+    
 
 };
 
